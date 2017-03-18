6da4d3c11707ca5545c7e618097a67db1858b904
Kenneth Bolivar <keboca@gmail.com>
2017-03-17 21:41:41 -0600

    Updated source code

--- src/java/NewServlet.java
+++ src/java/NewServlet.java
@@ -39,7 +39,7 @@ public class NewServlet extends HttpServlet {
             out.println("<title>Servlet NewServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet NewServlet executed through HTTP " + request.getMethod() + " method</h1>");
             out.println("</body>");
             out.println("</html>");
         }
